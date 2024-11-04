619ac9186624b7079f4eadd658ed12cd815b37a1
Mustafa Taha Demirci <mustafa@recurate.com>
2024-11-03 23:44:52 -0800

    switch divs to p

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -134,7 +134,7 @@ export default function Signup() {
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-              {formState.errors?.name && <div className="text-red-700">{formState.errors?.name}</div>} 
+              {formState.errors?.name && <p className="text-red-700">{formState.errors?.name}</p>} 
             </div>
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
@@ -172,7 +172,7 @@ export default function Signup() {
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-              {formState.errors?.password && <div className="text-red-700">{formState.errors?.password}</div>} 
+              {formState.errors?.password && <p className="text-red-700">{formState.errors?.password}</p>} 
             </div>
             <div>
               <div className="flex items-center justify-between">
